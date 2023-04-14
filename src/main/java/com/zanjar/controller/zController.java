364ab6427dad2fea3ed96ce7164d569f9d117149
Raque48 <jonathanfloress621@gmail.com>
2023-04-14 11:02:39 -0600

    first commit

--- src/main/java/com/zanjar/controller/zController.java
+++ src/main/java/com/zanjar/controller/zController.java
@@ -1,33 +1,34 @@
 package com.zanjar.controller;
 
 import org.springframework.stereotype.Controller;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/interfaces")
 public class zController{
 	
-	@RequestMapping("/index")
+	@GetMapping("/home")
 	public String index() {
 		return"interfaces/index";
 	}
 	
-	@RequestMapping("/formularioini")
+	@GetMapping("/ingresar")
 	public String formularioini() {
 		return"interfaces/formularioini";
 	}
 	
-	@RequestMapping("/formularioregis")
+	@GetMapping("/registrar")
 	public String formularioregis() {
 		return"interfaces/formularioregis";
 	}
 	
-	@RequestMapping("/perfilusuario")
+	@GetMapping("/perfil")
 	public String perfilusuario() {
 		return"interfaces/perfilusuario";
 	}
 	
-	@RequestMapping("/terminoslicencias")
+	@GetMapping("/terminos")
 	public String terminoslicencias() {
 		return"interfaces/terminoslicencias";
 	}
